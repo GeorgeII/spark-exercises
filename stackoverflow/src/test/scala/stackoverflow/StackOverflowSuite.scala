4e8550c6e0bea8557c87ffe06767cb3767097182
GeorgeII <egorzorikov@gmail.com>
2021-04-30 20:20:27 +0400

    Stackoverflow assignment, week 2

---
+++ stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
@@ -0,0 +1,54 @@
+package stackoverflow
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
+import org.junit._
+import org.junit.Assert.assertEquals
+import stackoverflow.StackOverflow.{groupedPostings, rawPostings, scoredPostings, vectorPostings}
+
+import java.io.File
+
+object StackOverflowSuite {
+  val conf: SparkConf = new SparkConf().setMaster("local").setAppName("StackOverflow")
+  val sc: SparkContext = new SparkContext(conf)
+}
+
+class StackOverflowSuite {
+  import StackOverflowSuite._
+
+
+  lazy val testObject = new StackOverflow {
+    override val langs =
+      List(
+        "JavaScript", "Java", "PHP", "Python", "C#", "C++", "Ruby", "CSS",
+        "Objective-C", "Perl", "Scala", "Haskell", "MATLAB", "Clojure", "Groovy")
+    override def langSpread = 50000
+    override def kmeansKernels = 45
+    override def kmeansEta: Double = 20.0D
+    override def kmeansMaxIterations = 120
+  }
+
+  @Test def `testObject can be instantiated`: Unit = {
+    val instantiatable = try {
+      testObject
+      true
+    } catch {
+      case _: Throwable => false
+    }
+    assert(instantiatable, "Can't instantiate a StackOverflow object")
+  }
+
+  @Test def `vectors must contain a certain number of elements`: Unit = {
+    val lines   = sc.textFile("src/main/resources/stackoverflow/stackoverflow.csv")
+    val raw     = rawPostings(lines)
+    val grouped = groupedPostings(raw)
+    val scored  = scoredPostings(grouped)
+    val vectors = vectorPostings(scored)
+    assertEquals(vectors.count(), 2121822)
+  }
+
+
+  @Rule def individualTestTimeout = new org.junit.rules.Timeout(100 * 1000)
+}
